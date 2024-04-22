3e4d4bc5d999719aaec69a8a29c6930ea391b5af
younome71 <mehtadarpans2004@gmail.com>
2024-04-22 19:02:02 +0530

    Initial Commit

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const register = (req,res) =>{
+    const { username, email, password } = req.body;
+    //HASH THE PASSWORD
+    //CREATE A NEW USER and SAVE TO DB
+}
+
+export const login = (req,res) =>{
+    //db operations
+}
+export const logout = (req,res) =>{
+    //db operations
+}
